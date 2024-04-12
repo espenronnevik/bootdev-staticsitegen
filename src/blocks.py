42b8f864ae476ef0bb14fc44facfcd7a495c60ac
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Add the various block types

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,10 @@
+block_type_paragraph      = "paragraph"
+block_type_heading        = "heading"
+block_type_code           = "code"
+block_type_quote          = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list   = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = []
     for line in markdown.split("\n\n"):
