719fa23fe844a67b58c9db60173d122145aa7b29
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Implement TextNode class

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
-print("hello world")
+from textnode import TextNode
 
+def main():
+    bold = TextNode("testing", "bold")
+    print(bold)
+    image = TextNode("testing image", "link", "http://example.com/test.jpg")
+    print(image)
+    clone = TextNode("testing image", "link", "http://example.com/test.jpg")
+
+    if image == clone:
+        print("Image and it's clone are equal")
+    else:
+        print("Image and it's clone are not equal")
+
+
+main()
