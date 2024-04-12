514e4f6e5b8d03106ac60603de353630d741347b
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Fix some logical errors in link/image function. Rename new_parts to new_nodes for clarity/sanity

--- src/textnode.py
+++ src/textnode.py
@@ -16,7 +16,7 @@ class TextNode(object):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_parts = []
+    new_nodes = []
 
     if len(old_nodes) == 0:
         return []
@@ -40,12 +40,12 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_node = TextNode(node_parts[i], "text")
             else:
                 new_node = TextNode(node_parts[i], text_type)
-            new_parts.append(new_node)
+            new_nodes.append(new_node)
     else:
-        new_parts.append(node)
+        new_nodes.append(node)
 
-    new_parts.extend(split_nodes_delimiter(old_nodes[1:], delimiter, text_type))
-    return new_parts
+    new_nodes.extend(split_nodes_delimiter(old_nodes[1:], delimiter, text_type))
+    return new_nodes
 
 def extract_markdown_images(text):
     re_image = r"!\[(.*?)\]\((.*?)\)"
@@ -55,29 +55,38 @@ def extract_markdown_links(text):
     re_links = r"\[(.*?)\]\((.*?)\)"
     return re.findall(re_links, text)
 
-def split_nodes_image(old_nodes):
+def split_nodes_images(old_nodes):
     new_nodes = []
 
     if len(old_nodes) == 0:
         return []
 
     try:
-        text = old_nodes[0].text
+        node = old_nodes[0]
+        node_text = node.text
+        node_text_type = node.text_type
     except:
         raise TypeError(f"{old_nodes[0]}is not a TextNode")
 
+    if node_text_type == "text":
+        worktext = node_text
+        extracted = extract_markdown_images(node_text)
+        if len(extracted) > 0:
+            for image in extracted:
+                md = f"![{image[0]}]({image[1]})"
+                parts = worktext.partition(md)
+                if parts[0] != "":
+                    new_nodes.append(TextNode(parts[0], "text"))
+                new_nodes.append(TextNode(image[0], "image", image[1]))
+                worktext = parts[2]
+            if worktext != "":
+                new_nodes.append(TextNode(worktext, "text"))
+        else:
+            new_nodes.append(node)
+    else:
+        new_nodes.append(node)
 
-    for image in extract_markdown_images(text):
-        new_parts = []
-        md = f"![{image[0]}]({image[1]})"
-        parts = text.partition(md)
-        if parts[0] != "":
-           new_parts.append(TextNode(parts[0], "text"))
-        new_parts.append(TextNode(image[0], "image", image[1]))
-        new_nodes.extend(new_parts)
-        text = parts[2]
-
-    new_nodes.extend(split_nodes_image(old_nodes[1:]))
+    new_nodes.extend(split_nodes_images(old_nodes[1:]))
     return new_nodes
 
 def split_nodes_links(old_nodes):
@@ -87,19 +96,29 @@ def split_nodes_links(old_nodes):
         return []
 
     try:
-        text = old_nodes[0].text
+        node = old_nodes[0]
+        node_text = node.text
+        node_text_type = node.text_type
     except:
         raise TypeError(f"{old_nodes[0]}is not a TextNode")
 
-    for link in extract_markdown_links(text):
-        new_parts = []
-        md = f"[{link[0]}]({link[1]})"
-        parts = text.partition(md)
-        if parts[0] != "":
-           new_parts.append(TextNode(parts[0], "text"))
-        new_parts.append(TextNode(link[0], "link", link[1]))
-        new_nodes.extend(new_parts)
-        text = parts[2]
+    if node_text_type == "text":
+        worktext = node_text
+        extracted = extract_markdown_links(node_text)
+        if len(extracted) > 0:
+            for link in extracted:
+                md = f"[{link[0]}]({link[1]})"
+                parts = worktext.partition(md)
+                if parts[0] != "":
+                    new_nodes.append(TextNode(parts[0], "text"))
+                new_nodes.append(TextNode(link[0], "link", link[1]))
+                worktext = parts[2]
+            if worktext != "":
+                new_nodes.append(TextNode(worktext, "text"))
+        else:
+            new_nodes.append(node)
+    else:
+        new_nodes.append(node)
 
     new_nodes.extend(split_nodes_links(old_nodes[1:]))
     return new_nodes
