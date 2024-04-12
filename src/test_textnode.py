bcfdf2245cfe05f54421486b14825162259f563f
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Implement split_nodes_delimiter() and corresponding unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_create_url_optional(self):
@@ -24,6 +24,40 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is an image node", "link", "http://example.com/test.jpg")
         self.assertNotEqual(node, node2)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_textsplit_codeblock(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        res = [TextNode("This is text with a ", "text"), TextNode("code block", "code"), TextNode(" word", "text")]
+        self.assertListEqual(split_nodes_delimiter([node], "`", "code"), res)
+
+    def test_textsplit_bold(self):
+        node = TextNode("This is text with a **bolded** word", "text")
+        res = [TextNode("This is text with a ", "text"), TextNode("bolded", "bold"), TextNode(" word", "text")]
+        self.assertListEqual(split_nodes_delimiter([node], "**", "bold"), res)
+
+    def test_textsplit_italic(self):
+        node = TextNode("This is text with a *italicized* word", "text")
+        res = [TextNode("This is text with a ", "text"), TextNode("italicized", "italic"), TextNode(" word", "text")]
+        self.assertListEqual(split_nodes_delimiter([node], "*", "italic"), res)
+
+    def test_textsplit_notextnode(self):
+        with self.assertRaises(TypeError):
+            split_nodes_delimiter(["fake TextNode"], "*", "italic")
+
+    def test_textsplit_nolist(self):
+        with self.assertRaises(TypeError):
+            split_nodes_delimiter("fake TextNode", "**", "bold")
+
+    def test_unclosed_symbols(self):
+        node = TextNode("This is text with an uneven **bold symbol", "text")
+        with self.assertRaises(SyntaxError):
+            split_nodes_delimiter([node], "**", "bold")
+
+    def test_multi_symbols(self):
+        node = TextNode("**Double** the **boldness**", "text")
+        res = [TextNode("Double", "bold"), TextNode(" the ", "text"), TextNode("boldness", "bold")]
+        self.assertListEqual(split_nodes_delimiter([node], "**", "bold"), res)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -12,3 +12,35 @@ class TextNode(object):
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_parts = []
+
+    if len(old_nodes) == 0:
+        return []
+
+    try:
+        node = old_nodes[0]
+        node_text_type = node.text_type
+        node_parts = node.text.split(delimiter)
+    except:
+        raise TypeError(f"{old_nodes[0]}is not a TextNode")
+
+    parts_length = len(node_parts)
+    if parts_length % 2 == 0:
+        raise SyntaxError("Invalid markdown syntax")
+
+    if node_text_type == "text":
+        for i in range(parts_length):
+            if node_parts[i] == "":
+                continue
+            if i % 2 == 0:
+                new_node = TextNode(node_parts[i], "text")
+            else:
+                new_node = TextNode(node_parts[i], text_type)
+            new_parts.append(new_node)
+    else:
+        new_parts.append(node)
+
+    new_parts.extend(split_nodes_delimiter(old_nodes[1:], delimiter, text_type))
+    return new_parts
