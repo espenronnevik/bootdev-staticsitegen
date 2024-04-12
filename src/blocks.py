f25f578ad753943234fa8b275d867d9894748c0c
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Start implementing block_to_block_type()

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,5 @@
+from re import match, findall
+
 block_type_paragraph      = "paragraph"
 block_type_heading        = "heading"
 block_type_code           = "code"
@@ -11,3 +13,23 @@ def markdown_to_blocks(markdown):
         if line != "":
             blocks.append(line.strip())
     return blocks
+
+def block_to_block_type(text)
+    re_heading = r"^#{1,6} "
+    re_code = r"(?s)^```.*```$"
+    re_quote = r"(?m)^(>)"
+    re_unordered_list = r"(?m)^(\*|-) "
+    re_ordered_list = r"(?m)^(\d+)\. "
+
+    type = block_type_paragraph
+    lines = text.split("\n")
+    if match(re_heading, text) is not None:
+        type = block_type_heading
+    elif match(re_code, text) is not None:
+        type = block_type_code
+    elif len(findall(re_quote, text)) == len(lines):
+        type = block_type_quote
+    elif len(findall(re_unordered_list, text)) == len(lines):
+        type = block_type_unordered_list
+    elif len(findall(re_ordered_list, text)) == len(lines):
+        type = block_type_ordered_list
