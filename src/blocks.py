ee40e4496006157295ac61e58957cecf3c68d72d
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Do rest of block_to_block_type() and a helper for ordered lists. Write a whole bunch of tests

--- src/blocks.py
+++ src/blocks.py
@@ -14,7 +14,15 @@ def markdown_to_blocks(markdown):
             blocks.append(line.strip())
     return blocks
 
-def block_to_block_type(text)
+def validate_order(lines, num=1):
+    if len(lines) == 0:
+        return True
+
+    snum = lines[0].split(".")[0]
+    res =  int(snum) == num
+    return res and validate_order(lines[1:], num + 1)
+
+def block_to_block_type(text):
     re_heading = r"^#{1,6} "
     re_code = r"(?s)^```.*```$"
     re_quote = r"(?m)^(>)"
@@ -31,5 +39,6 @@ def block_to_block_type(text)
         type = block_type_quote
     elif len(findall(re_unordered_list, text)) == len(lines):
         type = block_type_unordered_list
-    elif len(findall(re_ordered_list, text)) == len(lines):
+    elif len(findall(re_ordered_list, text)) == len(lines) and validate_order(lines):
         type = block_type_ordered_list
+    return type
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,7 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type
+from blocks import block_type_paragraph, block_type_heading, block_type_code, block_type_quote, block_type_unordered_list, block_type_ordered_list
 
 class TestBlockFuncs(unittest.TestCase):
     def test_markdown_to_blocks_example(self):
@@ -14,3 +15,57 @@ class TestBlockFuncs(unittest.TestCase):
     def test_markdown_to_blocks_empty(self):
         text = "This is **two** blocks of text\n\n\n\n\n\n\n\nWith excessive newlines in between"
         self.assertEqual(len(markdown_to_blocks(text)), 2)
+
+    def test_block_to_block_type_paragraph(self):
+        text = "This is just a normal paragraph"
+        self.assertEqual(block_to_block_type(text), block_type_paragraph)
+
+    def test_block_to_block_type_heading(self):
+        valid = "### A Level 3 header"
+        invalid = "No header here"
+        fake = "This appears to contain headers\n# But it does not"
+        self.assertEqual(block_to_block_type(valid), block_type_heading)
+        self.assertEqual(block_to_block_type(invalid), block_type_paragraph)
+        self.assertEqual(block_to_block_type(fake), block_type_paragraph)
+
+    def test_block_to_block_type_code(self):
+        valid_single = "``` Valid single line code block ```"
+        valid_multi = "```This is a valid\nMultiline code block```"
+        invalid = "``This only contains two backticks``"
+        unbalanced = "```Valid beginning tag\nbut it in unbalanced and missing end tag"
+        self.assertEqual(block_to_block_type(valid_single), block_type_code)
+        self.assertEqual(block_to_block_type(valid_multi), block_type_code)
+        self.assertEqual(block_to_block_type(invalid), block_type_paragraph)
+        self.assertEqual(block_to_block_type(unbalanced), block_type_paragraph)
+
+    def test_block_to_block_type_quote(self):
+        valid_single = "> This is one liner"
+        valid_multi = "> Quote spanning\n>over several\n>lines"
+        fake = "The character should not > appear in the middle"
+        invalid_multi = "> All lines \n>should contain\na > character at the start"
+        self.assertEqual(block_to_block_type(valid_single), block_type_quote)
+        self.assertEqual(block_to_block_type(valid_multi), block_type_quote)
+        self.assertEqual(block_to_block_type(fake), block_type_paragraph)
+        self.assertEqual(block_to_block_type(invalid_multi), block_type_paragraph)
+
+    def test_block_to_block_type_unordered_list(self):
+        valid_single_var1 ="* This is one version"
+        valid_single_var2 = "- This is another version"
+        valid_mixed = "* Mixing and matching\n- variants is possible"
+        valid_multi_long = "* A big \n* unordered\n* List\n* With many lines"
+        invalid = "- Every line must contain\n a list character"
+        self.assertEqual(block_to_block_type(valid_single_var1), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(valid_single_var2), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(valid_mixed), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(valid_multi_long), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(invalid), block_type_paragraph)
+
+    def test_block_to_block_type_ordered_list(self):
+        valid_single = "1. This is an ordered list"
+        valid_multi = "1. First item\n2. Second item\n3. Third item"
+        invalid_multi = "1. First item\n Second item\n3. Third item"
+        invalid_order = "1. First item\n3. Second item\n2. Third item"
+        self.assertEqual(block_to_block_type(valid_single), block_type_ordered_list)
+        self.assertEqual(block_to_block_type(valid_multi), block_type_ordered_list)
+        self.assertEqual(block_to_block_type(invalid_multi), block_type_paragraph)
+        self.assertEqual(block_to_block_type(invalid_order), block_type_paragraph)
